59e483a6940b0e812a0da8142a03d124be7058ff
25ALN <3076331747@qq.com>
2024-12-24 23:21:33 +0800

    work

--- student.c
+++ student.c
@@ -19,10 +19,65 @@ void searchname();
 void studentin();
 void studentdelete();
 void studentpaixu();
+void printneed()
+{
+    printf("学生信息管理系统\n");
+    printf("1. 输入学生信息\n");
+    printf("2. 输出学生信息\n");
+    printf("3. 按学号查询学生信息\n");
+    printf("4. 按姓名查询学生信息\n");
+    printf("5. 插入学生信息\n");
+    printf("6. 删除学生信息\n");
+    printf("7. 按姓名排序学生信息\n");
+    printf("8. 保存学生信息到文件\n");
+    printf("9. 从文件加载学生信息\n");
+    printf("0. 退出系统\n");
+}
 int main()
 {
-    FILE *fp = ("stu.txt", "r");
+    FILE *fp = fopen("stu.txt", "r");
+    char str[100000];
+    while (fgets(str, sizeof(str), fp) != NULL)
+        studentnum++;
+    void printneed();
+    int choice,l=1;
+    scanf("%d", &choice);
+    while (l==1)
+    {
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            messageout();
+            break;
+        case 2:
+            messagein();
+            break;
+        case 3:
+            searchxueahao();
+            break;
+        case 4:
+            searchname();
+            break;
+        case 5:
+            studentin();
+            break;
+        case 6:
+            studentdelete();
+            break;
+        case 7:
+            studentpaixu();
+            break;
+        case 8:
 
+            break;
+        case 9:
+            l=0;
+            break;
+        default:
+            break;
+        }
+    }
     return 0;
 }
 void messagein()
@@ -110,30 +165,47 @@ void studentin()
         scanf("%d", &student[studentnum].grade[j]);
     }
 }
-void studentdelete(){
-    if(studentnum==0){
+void studentdelete()
+{
+    if (studentnum == 0)
+    {
         printf("没有任何学生的信息");
         return;
     }
     char nm[10];
     printf("请输入你想删除的学生姓名\n");
-    scanf("%s",nm);
-    for(int i=0;i<studentnum;i++){
-        if(strcmp(nm,student[i].name)==0){
-            for(int j=i;j<studentnum-1;j++){
-                student[j]=student[j+1];
+    scanf("%s", nm);
+    for (int i = 0; i < studentnum; i++)
+    {
+        if (strcmp(nm, student[i].name) == 0)
+        {
+            for (int j = i; j < studentnum - 1; j++)
+            {
+                student[j] = student[j + 1];
             }
+            studentnum--;
             printf("该生信息已删除");
             return;
         }
     }
     printf("未找到该学生相关信息");
 }
-void studentpaixu(){
-    for(int i=0;i<studentnum;i++){
-        for(int j=i;j<studentnum;j++){
-            if(strcmp(student[i].name,student[j].name)){
-                
+void studentpaixu()
+{
+    if (studentnum == 0)
+    {
+        printf("未找到任何学生相关信息");
+        return;
+    }
+    for (int i = 0; i < studentnum; i++)
+    {
+        for (int j = i; j < studentnum; j++)
+        {
+            if (strcmp(student[i].name, student[j].name) <= 0)
+            {
+                struct s uo = student[i];
+                student[i] = student[j];
+                student[j] = uo;
             }
         }
     }
